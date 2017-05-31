2def020f0f0cc4053586eef17de36a0d86f83ac8
sarahfeldmann <sarahf95@gmail.com>
2017-05-31 16:38:50 -0700

    not sure

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var trialData = []; //Stores data for a trial before it is written to output
 $(".phrases").hide();
 var rawText = $(".phrases").text();
 var phrases = rawText.split('\n');
-for( var i = 0; i < phrases.length; i++) {
-    phrases[i] = phrases[i].trim();    
+for (var i = 0; i < phrases.length; i++) {
+    phrases[i] = phrases[i].trim();
 }
 
 //  hide homepage when button pressed
@@ -20,11 +20,12 @@ $(document).ready(function () {
 });
 
 // "send" text 
-// var textbox = document.getElementById("write");
-// var sendbutton = document.getElementsByClassName("send");
-$(".send").click(function() {
-    $("#write").val('');
+$(document).ready(function () {
+    $(".send").click(function () {
+        $("#write").val('');
+    });
 });
+
 
 
 
@@ -92,10 +93,10 @@ $(document).ready(function () {
 $('.lowercase').hide(); //Hides lowercase letters initially
 
 //Toggles caps and nomal keyboard
-$('#write').toggle(function() {
+$('#write').toggle(function () {
     $('.home, .middle').hide();
     $('.caps').show();
-}, function() {
+}, function () {
     $('.home, .middle').show();
     $('.caps').hide();
 });
@@ -132,39 +133,39 @@ $(document).ready(function () {
 
 
 // "typing" for keyboard
-$(function(){
+$(function () {
     var $write = $('#write');
 
     //Typing with drag
-    $('.char').mouseup(function(){
+    $('.char').mouseup(function () {
         var $this = $(this),
-        character = $this.html(); 
+            character = $this.html();
         $(".lowercase").show();
 
         // Add the character
         $write.html($write.html() + character);
-            
+
         $($this.parent().parent()).hide();
         $('.home, .middle').show();
 
         //records data if after practice trials
-        if(keystrokes) {
+        if (keystrokes) {
             recordKeyData(character);
         }
     });
 });
 
 //Space bar functionality
-$(function(){
-    var $write = $('#write');   
+$(function () {
+    var $write = $('#write');
     var character = ' ';
     //Typing with click 
-    $('#space').mousedown(function(){
+    $('#space').mousedown(function () {
         $write.html($write.html() + character);
     });
-    
+
     //Records data if after practice trials
-    if(keystrokes) {
+    if (keystrokes) {
         recordKeyData(character);
     }
 });
@@ -172,11 +173,11 @@ $(function(){
 //Backspace functionality: swipe left to backspace
 var textbox = document.getElementById("write");
 var inputbox = new Hammer(textbox);
-inputbox.on("swipeleft", function() {
+inputbox.on("swipeleft", function () {
     var temp = textbox.innerHTML;
-    var length = temp.length -  1;
+    var length = temp.length - 1;
     textbox.innerHTML = temp.substring(0, length);
-    if(keystrokes) {
+    if (keystrokes) {
         recordKeyData("&#x8");
     }
 });
@@ -186,7 +187,7 @@ inputbox.on("swipeleft", function() {
 var homepage = document.getElementsByClassName("homepage");
 var numbers = document.getElementsByClassName("numbers");
 var mainpage = new Hammer(homepage);
-mainpage.on("swiperight", function() {
+mainpage.on("swiperight", function () {
     mainpage.hide();
     numbers.show();
 });
@@ -195,7 +196,7 @@ mainpage.on("swiperight", function() {
 var homepage = document.getElementsByClassName("homepage");
 var punctuation1 = document.getElementsByClassName("symbols1");
 var mainpage = new Hammer(homepage);
-mainpage.on("swipeleft", function() {
+mainpage.on("swipeleft", function () {
     mainpage.hide();
     punctuation1.show();
 });
@@ -204,7 +205,7 @@ mainpage.on("swipeleft", function() {
 var symbols = document.getElementsByClassName("symbols1");
 var punctuation2 = document.getElementsByClassName("symbols2");
 var mainpage = new Hammer(symbols);
-mainpage.on("swipeleft", function() {
+mainpage.on("swipeleft", function () {
     punctuation1.hide();
     punctuation2.show();
 });
@@ -213,49 +214,49 @@ mainpage.on("swipeleft", function() {
 
 
 //Swipe right on input box to enter text
-inputbox.on("swiperight", function() {     
+inputbox.on("swiperight", function () {
     count++;
 
     //Records data at the end of a trial     
-    if(keystrokes && count > 5) {
-        var transcribed = $(".input").text();      
+    if (keystrokes && count > 5) {
+        var transcribed = $(".input").text();
         var results = $(".results");
         results.append(document.createTextNode("<Trial number=\"" + (count - 5) + "\" testing=\"false\" entries=\"" + (trialData.length - 1) + "\">"));
-        results.append('</br>');  
+        results.append('</br>');
         for (var i = 0; i < trialData.length; i++) {
             results.append(document.createTextNode(trialData[i]));
-            results.append('</br>');              
+            results.append('</br>');
         }
         results.append(document.createTextNode("<Transcribed>" + transcribed + "</Transcribed>"));
-        results.append('</br>');  
+        results.append('</br>');
         results.append(document.createTextNode("</Trial>"));
-        results.append('</br>'); 
+        results.append('</br>');
         trialData = [];
     }
 
-    if(test) {       
-        
+    if (test) {
+
         //Creates testing information once practice trials have been completed
-        if(count == 5) {
+        if (count == 5) {
             var d = new Date();
             var date = d.toLocaleString();
             var ticks = ((d.getTime() * 10000) + 621355968000000000);
-            var seconds = ticks / 10000000;      
+            var seconds = ticks / 10000000;
             var results = $(".results");
-            results.append(document.createTextNode("<TextTest version=\"2.7.2\" trials=\"" + numTrials + "\" ticks=\"" + ticks + "\" seconds=\"" + seconds + "\" date=\"" + date + "\">"));    
+            results.append(document.createTextNode("<TextTest version=\"2.7.2\" trials=\"" + numTrials + "\" ticks=\"" + ticks + "\" seconds=\"" + seconds + "\" date=\"" + date + "\">"));
             results.append('</br>');
-            keystrokes = true;        
-        } 
-        
+            keystrokes = true;
+        }
+
         //If test is finished
-        if(count >= (numTrials + 5)) {
+        if (count >= (numTrials + 5)) {
             var results = $(".results");
             results.append(document.createTextNode("</TextTest>"));
             $(".prompt").hide();
             $(".resultssection").show();
             test = false;
-            keystrokes = false; 
-            $(".input").empty();     
+            keystrokes = false;
+            $(".input").empty();
         } else {
             showNextQuestion();
         }
@@ -276,25 +277,25 @@ function textTest() {
 
 //Shows a new phrase
 function showNextQuestion() {
-                
-        $(".input").empty();
-        var singlePhrase = phrases[Math.floor((Math.random() * phrases.length))]
-        $(".promptText").text(singlePhrase); 
-        
-        //records data at beginning of trial
-        if(keystrokes) {
-            trialData.push("<Presented>" + singlePhrase + "</Presented>");           
-        } 
+
+    $(".input").empty();
+    var singlePhrase = phrases[Math.floor((Math.random() * phrases.length))]
+    $(".promptText").text(singlePhrase);
+
+    //records data at beginning of trial
+    if (keystrokes) {
+        trialData.push("<Presented>" + singlePhrase + "</Presented>");
+    }
 }
 
 //Records keystroke data about a given character
 function recordKeyData(key) {
-            var d = new Date();
-            var ticks = ((d.getTime() * 10000) + 621355968000000000);
-            var seconds = ticks / 10000000;               
-            var ascii = key.charCodeAt(0)
-            if(key == "&#x8") {
-                acii = 8;
-            }
-            trialData.push("<Entry char=\"" + key + "\" value=\"" + ascii + "\" ticks=\"" + ticks + "\" seconds=\"" + seconds + "\" />");      
+    var d = new Date();
+    var ticks = ((d.getTime() * 10000) + 621355968000000000);
+    var seconds = ticks / 10000000;
+    var ascii = key.charCodeAt(0)
+    if (key == "&#x8") {
+        acii = 8;
+    }
+    trialData.push("<Entry char=\"" + key + "\" value=\"" + ascii + "\" ticks=\"" + ticks + "\" seconds=\"" + seconds + "\" />");
 }
